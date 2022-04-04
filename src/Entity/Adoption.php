51b83772f4a42a9c8d0845b914d907a288198e69
Alaric-Gautier <alaric.gautier@hotmail.fr>
2022-04-04 15:16:30 +0200

    new migration

--- src/Entity/Adoption.php
+++ src/Entity/Adoption.php
@@ -95,9 +95,15 @@ class Adoption
      */
     private $animaux_proposes;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Animal::class, mappedBy="adoption")
+     */
+    private $animals;
+
     public function __construct()
     {
         $this->membres = new ArrayCollection();
+        $this->animals = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -296,6 +302,33 @@ class Adoption
     public function setAnimauxProposes(?string $animaux_proposes): self
     {
         $this->animaux_proposes = $animaux_proposes;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Animal>
+     */
+    public function getAnimals(): Collection
+    {
+        return $this->animals;
+    }
+
+    public function addAnimal(Animal $animal): self
+    {
+        if (!$this->animals->contains($animal)) {
+            $this->animals[] = $animal;
+            $animal->addAdoption($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnimal(Animal $animal): self
+    {
+        if ($this->animals->removeElement($animal)) {
+            $animal->removeAdoption($this);
+        }
 
         return $this;
     }
