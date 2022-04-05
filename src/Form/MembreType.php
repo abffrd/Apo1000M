c403a4b6e80a126c226af56ae0e85773349ec6b4
Vincent-Goicoechea <vgoico7@gmail.com>
2022-04-05 12:24:52 +0200

    update add member form

--- src/Form/MembreType.php
+++ src/Form/MembreType.php
@@ -7,6 +7,10 @@ use App\Entity\Membre;
 use App\Entity\Role;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,13 +19,17 @@ class MembreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('mot_de_passe')
-            ->add('identifiant')
-            ->add('actif')
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'])
+            ->add('mot_de_passe', PasswordType::class)
+            ->add('identifiant', TextType::class)
+            ->add('actif', CheckboxType::class)
+            
+            //TODO ajout possibilité de sélectionner plusieurs roles
             ->add('roles', EntityType::class, [
                 'class'=>Role::class,
+                'label' => 'Rôles',
                 'choice_label' => 'role',
                 'multiple' => false,
                 'expanded' => false,
@@ -29,12 +37,12 @@ class MembreType extends AbstractType
             ])
             ->add('adoptions', EntityType::class, [
                 'class'=>Adoption::class,
-                'choice_label' => 'adoptant',
+                'choice_label' => 'id',
                 'multiple' => false,
                 'expanded' => false,
                 'mapped' => false,
-            ])
-        ;
+            ]);
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
