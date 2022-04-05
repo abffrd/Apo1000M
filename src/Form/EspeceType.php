1ff907753f45f82dbe4b6885b205c91cfc0836b3
Alaric-Gautier <alaric.gautier@hotmail.fr>
2022-04-05 11:35:27 +0200

    added CRUD for animals and species

---
+++ src/Form/EspeceType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Espece;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EspeceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type')
+            ->add('familleAccueils')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Espece::class,
+        ]);
+    }
+}
