54ca19fbed747aeda4336143e8aba508206fe61b
Vincent-Goicoechea <vgoico7@gmail.com>
2022-04-11 16:45:29 +0200

    test make:auth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,34 +7,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class LoginController extends AbstractController
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/", name="app_login")
      */
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Les deux lignes suivantes servent en cas d'échec à la connexion
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
-     * Logout
-     * 
      * @Route("/logout", name="app_logout")
      */
-    public function logout()
+    public function logout(): void
     {
-        // Ce code ne sera jamais exécuté
-        // le composant de sécurité va intercepter la requête avant.
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
 }
